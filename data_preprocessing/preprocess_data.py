344c166e1ab750f251fc44835ba1c9ccc11a041b
James Wong <jmswong12@gmail.com>
2022-11-12 23:01:33 -0800

    Join corrosion and output data

--- data_preprocessing/preprocess_data.py
+++ data_preprocessing/preprocess_data.py
@@ -1,6 +1,8 @@
 import argparse
 import corrosion_lib
+import numpy as np
 import output_lib
+import re
 
 parser = argparse.ArgumentParser(description="Extract and process corrosion and output files")
 parser.add_argument('--output_path', help='Path to location of corrosion data. If --extract=True, the extracted files will also be extracted to here.')
@@ -13,6 +15,39 @@ args = parser.parse_args()
 
 print(args)
 
+def join_corrosion_and_outputs(corrosion_maps, output_maps):
+  corrosion_output = []
+  target_output = []
+  for file_path, corrosion_map in corrosion_maps:
+    file_name = file_path.split("/")[-1]
+    m = re.search("Corrosion_simulation_(\d+)_timeStep_(\d+).txt", file_name)
+    simulation_idx, timestep = int(m.group(1)), int(m.group(2))
+    output_filename = "output_%d_%d.mat" % (simulation_idx, timestep)
+
+    # find the corresponding output map
+    target_label = None
+    for output_path, output_map in output_maps:
+      if output_path.split("/")[-1] == output_filename:
+        target_label = output_map['label']
+        break
+    if target_label is None:
+      print("Skipping simulation %d timestep %d since output is missing" % (simulation_idx, timestep))
+      continue
+
+    target_output.append(target_label)
+
+    # Since we've verified the x-axis evenly distributed and are are all on the
+    # same scale, we can drop the points on the x-axis and represent the
+    # corrosion depths for a single timestep as a vector in 1d or matrix in 2d.
+    corrosion_depths = list(corrosion_map.values())
+
+    # We will let the first and second columns of the output represent the simulation
+    # number and timestep respectively.
+    corrosion_output.append([simulation_idx, timestep] + corrosion_depths)
+
+  assert len(corrosion_output) == len(target_output) 
+  return np.array(corrosion_output), np.array(target_output)
+
 def preprocess():
   # Extract the first num_simulations experiments to output_path.
   if (args.extract):
@@ -22,17 +57,18 @@ def preprocess():
   corrosion_maps = corrosion_lib.extract_1d_corrosion_maps(args.output_path, args.num_simulations)
   output_maps = output_lib.extract_concrete_outputs(args.output_path, args.num_simulations)
  
-  # Rescale corrosion depths to all be on the same scale
+  # Rescale corrosion depths to all be on the same scale. Also replace any corrosion depths from outputs.
   corrosion_maps = corrosion_lib.remap_output_scales(corrosion_maps, output_maps)
   
   # check that all corrosion datapoints are on the same rebar scale
   assert corrosion_lib.verify_rebar_locations(corrosion_maps)
  
   # join corrosion and output data 
+  corrosion_array, target_array = join_corrosion_and_outputs(corrosion_maps, output_maps)
+  print("corrosion_array shape: ", corrosion_array.shape)
+  print("target_array shape: ", target_array.shape)
 
-  print(len(corrosion_maps), len(output_maps))
-  import pdb; pdb.set_trace()
-
+  # save ndarray to file
 
 
 if __name__ == "__main__":
