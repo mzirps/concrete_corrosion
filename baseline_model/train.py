cd34970e50d6ed3e95ac00ae9d9787e05e610fb3
James Wong <jmswong12@gmail.com>
2022-11-15 00:46:49 -0800

    baseline train.py comments

--- baseline_model/train.py
+++ baseline_model/train.py
@@ -1,3 +1,5 @@
+"""Baseline Convolution+FullyConnected model"""
+
 import argparse
 import copy
 import random
@@ -5,8 +7,8 @@ import torch
 
 import numpy as np
 
-import torch.nn as nn
 import torch.nn.functional as F
+from torch import nn
 from torch.utils.data import Dataset, DataLoader
 
 import sklearn
@@ -17,15 +19,22 @@ from sklearn.metrics import recall_score
 from sklearn.model_selection import train_test_split
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--corrosion_path', default='/home/wongjames/cs230/Project/data_11_09_2022/corrosion.npy', help="Path of saved corrosion numpy array")
-parser.add_argument('--label_path', default='/home/wongjames/cs230/Project/data_11_09_2022/target_labels.npy', help="Path of saved target label numpy array")
-parser.add_argument('--output_path', default='/home/wongjames/cs230/Project/models/baseline_model.pt', help="Path to save trained pytorch model state")
+parser.add_argument('--corrosion_path',
+                    default='/home/wongjames/cs230/Project/data_11_09_2022/corrosion.npy',
+                    help="Path of saved corrosion numpy array")
+parser.add_argument('--label_path',
+                    default='/home/wongjames/cs230/Project/data_11_09_2022/target_labels.npy',
+                    help="Path of saved target label numpy array")
+parser.add_argument('--output_path',
+                    default='/home/wongjames/cs230/Project/models/baseline_model.pt',
+                    help="Path to save trained pytorch model state")
 parser.add_argument('--batch_size', type=int, default=1, help="Batch size to use for training")
 parser.add_argument('--num_epochs', type=int, default=100, help="Number of training epochs")
 parser.add_argument('--learning_rate', type=float, default=0.01, help="Learning rate")
 
 args = parser.parse_args()
 
+# Data class for corrosion data, concrete properties, and target label
 class Data(Dataset):
   def __init__(self, corrosion_data, target_labels):
     # The first 2 columns are the simulation_idx and timestep respectively,
@@ -37,27 +46,29 @@ class Data(Dataset):
     self.concrete_inputs = torch.from_numpy(corrosion_data[:, 2:6].astype(np.float32))
     self.target_labels = torch.from_numpy(target_labels.astype(np.float32))
     self.len = self.corrosion_inputs.shape[0]
-       
+
   def __getitem__(self, index):
     return self.corrosion_inputs[index], self.concrete_inputs[index], self.target_labels[index]
-   
+
   def __len__(self):
     return self.len
 
+# Baseline Convolution + FC model.
+# This model runs corrosion depths through a single-layer 1d convolution with
+# one kernel of size 20, followed by ReLU and max-pooling with stride 16.
+# Then the output is concatenated with the 4 continuous concrete-property
+# features, and fed into a fully connected layer, with sigmoid activation.
 class CNN1FC1(nn.Module):
 
   def __init__(self):
     super(CNN1FC1, self).__init__()
-    # 1 input image channel, 2 output channels, 5x1 convolution kernel
-    # input: (batch x 1 x 337)
-    # output: (batch x 2 x 333)
+    # 1 input image channel, 1 output channel, 20x1 convolution kernel
     self.conv1 = nn.Conv1d(1, 1, 20)
 
     # fully connected layer, single output node
-    # input: (batch x (2 x 333 + 4))
-    # output: 1
     self.fc1 = nn.Linear(in_features = 23, out_features = 1, bias = True)
 
+  # Forward Prop
   # Inputs:
   #   corrosion_depths: tensor of dim (batch_size x 337)
   #   concrete_features: tensor of dim (batch_size x 4)
@@ -65,22 +76,29 @@ class CNN1FC1(nn.Module):
   #    predictions: tensor of dim (batch_size x 1)
   def forward(self, corrosion_depths, concrete_features):
     corrosion_depths = torch.unsqueeze(corrosion_depths, 1)
+
+    # Input: batch x 1 x 337
+    # Output: batch x 1 x 318
     x = self.conv1(corrosion_depths)
+
     x = torch.nn.ReLU()(x)
 
-    # input: (batch x 2 x 333)
-    # output: (batch x 2 x 83)
+    # Input: (batch x 1 x 318)
+    # Output: (batch x 1 x 19)
     x = torch.nn.MaxPool1d(kernel_size=16, stride=16)(x)
-    x = torch.flatten(x, start_dim=1, end_dim=2) # batch x 170
 
-    # batch x 1 x 4 -> batch x 4
+    # Flatten to (batch x 19)
+    x = torch.flatten(x, start_dim=1, end_dim=2)
+
+    # Concat (batch x 19) with (batch x 4) -> (batch x 23)
     x = torch.concat([x, concrete_features], dim=1) #
 
     # fully connected layer
+    # Input: (batch x 23)
+    # Output: (batch x 1)
     x = self.fc1(x)
-      
-    return torch.sigmoid(x)
 
+    return torch.sigmoid(x)
 
 def compute_weighted_loss(pred, y, loss_fn):
   y = y.unsqueeze(-1)
@@ -102,12 +120,13 @@ if __name__ == '__main__':
 
   # Split to 70%/30% train/test sets
   random_state = 32
-  X_train, X_val, y_train, y_val = train_test_split(corrosion_data, target_data, test_size=0.3, random_state=random_state)
+  X_train, X_val, y_train, y_val = train_test_split(corrosion_data, target_data,
+                                                    test_size=0.3, random_state=random_state)
 
   # Instantiate training and test(validation) data
   train_data = Data(X_train, y_train)
   train_dataloader = DataLoader(dataset=train_data, batch_size=args.batch_size, shuffle=True)
-  
+
   # Create single-batch test data
   val_data = Data(X_val, y_val)
   val_dataloader = DataLoader(dataset=val_data, batch_size=X_val.shape[0], shuffle=True)
@@ -122,8 +141,8 @@ if __name__ == '__main__':
     for input1, input2, y in train_dataloader:
       # zero the parameter gradients
       optimizer.zero_grad()
-      
-      # forward prop 
+
+      # forward prop
       predictions = model(input1, input2)
 
       # compute weighted loss
@@ -132,12 +151,12 @@ if __name__ == '__main__':
         # Also compute validation loss
         validation_predictions = model(val_input1, val_input2)
         validation_avg_loss = compute_weighted_loss(validation_predictions, val_y, loss_fn)
-        print("Epoch %4d- Training Loss:%.5f   Validation Loss:%.5f" % (epoch, avg_loss, validation_avg_loss))
-      
-	  # compute gradients and update parameters
+        print(f"Epoch %4d- Training Loss:%.5f   Validation Loss:%.5f" % \
+              (epoch, avg_loss, validation_avg_loss))
+
+      # compute gradients and update parameters
       avg_loss.backward()
       optimizer.step()
 
   # Save model
   torch.save(model.state_dict(), args.output_path)
-
